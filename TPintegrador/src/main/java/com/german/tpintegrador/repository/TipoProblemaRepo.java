503e3c1351e1717196418e5eba3df0512327fcc2
Virginia <128263468+Virtar@users.noreply.github.com>
2023-11-29 21:51:08 -0300

    Cambios 29_11_2

--- TPintegrador/src/main/java/com/german/tpintegrador/repository/TipoProblemaRepo.java
+++
@@ -1,9 +0,0 @@
-package com.german.tpintegrador.repository;
-
-import com.german.tpintegrador.entity.TipoProblema;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TipoProblemaRepo extends JpaRepository<TipoProblema,Long> {
-}
